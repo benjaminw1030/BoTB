f04b72e82ac2f80df58af4597d77e4f053ede155
Messquerade <annac.klingberg@gmail.com>
2021-09-13 14:15:46 -0700

    add custom images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+// import Template from './js/template.js';
 
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
+// $(document).ready(function() {
+//   $('#triangle-checker-form').submit(function(event) {
+//     event.preventDefault();
 
-    $('#response').append("<p>" + response + "</p>");
-  });
-});
+//     $('#response').append("<p>" + response + "</p>");
+//   });
+// });
